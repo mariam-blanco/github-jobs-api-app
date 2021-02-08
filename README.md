14d98bd0136fdddd53f069d3c1fb0f2f5c4834a4
marianuskyes <mariamblanko@yahoo.es>
2021-02-08 01:43:13 +0100

    Modal mobile

--- README.md
+++ README.md
@@ -3,10 +3,10 @@
 ## General info
 This is a Frontend Mentor coding challenge. It consist in two parts:
 
-Part I. Build out a jobs board using the [GitHub Jobs API](https://jobs.github.com/api) data.
-Part II. Be able to click a job from the index page so that they can read more information and apply for the job. 
+1. **Part I**. Build out a jobs board using the [GitHub Jobs API](https://jobs.github.com/api) data.
+2. **Part II**. Be able to click a job from the index page so that they can read more information and apply for the job. 
 
-![Description for the GitHub Jobs API coding challenge](./src/assets/README.md)
+[Description for the GitHub Jobs API coding challenge](./src/assets/README.md)
 
 ![Design preview for the GitHub Jobs API coding challenge](./src/assets/preview.jpg)
 
--- src/js/main.js
+++ src/js/main.js
@@ -5,8 +5,18 @@ let numPage = 1;
 const MAX_CARDS_PAGE = 50;
 
 const form = document.querySelector(".js-form");
+const termsInputField = document.querySelector('.js-terms');
 const divCardsList = document.querySelector(".js-cards-list");
 const toggleSwitch = document.querySelector('.switch input[type="checkbox"]');
+
+const modal = document.querySelector('.js-modal');
+const btnOpenModal = document.querySelector('.js-btn-modal');
+const btnModalSearch = document.querySelector('.js-modal-submit-btn');
+
+/*
+* Change to "dark mode" or "light mode"
+* -----------------------------------------------------------
+*/
 
 const switchTheme = (e) => {
   if (e.target.checked) {
@@ -15,6 +25,20 @@ const switchTheme = (e) => {
     document.documentElement.setAttribute("data-theme", "ligth");
   }
 };
+
+const showModal = (e) => {
+  e.preventDefault();
+  modal.classList.remove('hidden');
+};
+
+const hideModal = () => {
+  modal.classList.add('hidden');
+}
+
+/*
+* URL to fetch data 
+* -----------------------------------------------------------
+*/
 
 const setURL = (terms, location, isFullTime) => {
   const corsURL = "https://cors-anywhere.herokuapp.com/";
@@ -37,6 +61,11 @@ const setURL = (terms, location, isFullTime) => {
   return url;
 };
 
+/*
+* Show error messages 
+* -----------------------------------------------------------
+*/
+
 const showMessage = (text) => {
   divCardsList.innerHTML = "";
   const paragraph = document.createElement("p");
@@ -46,19 +75,14 @@ const showMessage = (text) => {
   paragraph.appendChild(message);
 };
 
-const showLoadMoreBtn = () => {
-  const loadMoreBtn = document.createElement("button");
-  loadMoreBtn.className = "btn-primary js-loadMoreBtn";
-  const textBtn = document.createTextNode("Load more");
-  loadMoreBtn.appendChild(textBtn);
-  // loadMoreBtn.addEventListener('click', loadMoreCards);
-  loadMoreBtn.addEventListener("click", handleLoad);
-  divCardsList.after(loadMoreBtn);
-};
+/*
+* Fetch jobs offers from API
+* -----------------------------------------------------------
+*/
 
 const getJobs = async (terms, location, isFullTime) => {
   url = setURL(terms, location, isFullTime);
-  console.log(url);
+
   try {
     const response = await fetch(url);
     jobs = await response.json();
@@ -71,9 +95,49 @@ const getJobs = async (terms, location, isFullTime) => {
 };
 
 /*
-* renderCards() --> xxx 
+* Create "Load more" button
 * -----------------------------------------------------------
-* 
+*/
+
+const showLoadMoreBtn = () => {
+  const loadMoreBtn = document.createElement("button");
+  loadMoreBtn.className = "btn-primary js-loadMoreBtn";
+  const textBtn = document.createTextNode("Load more");
+  loadMoreBtn.appendChild(textBtn);
+  // loadMoreBtn.addEventListener('click', loadMoreCards);
+  loadMoreBtn.addEventListener("click", handleLoad);
+  divCardsList.after(loadMoreBtn);
+};
+
+/*
+* Calculate time since date of publication of job offer
+* -----------------------------------------------------------                    
+*/
+
+const calculateTime = (dateCreated) => {
+  const createdAtDate = new Date(dateCreated);
+  const nowDate = new Date();
+  const difference = nowDate - createdAtDate;
+
+  const hours = Math.floor(difference / (1000 * 3600));
+  if (hours < 24) {
+    return `${hours}h`;
+  }
+  const days = Math.floor(difference / (1000 * 3600 * 24));
+  if (days < 7) {
+    return `${days}d`;
+  }
+  const weeks = Math.floor(difference / (1000 * 3600 * 24 * 7));
+  if (weeks < 4) {
+    return `${weeks}w`;
+  }
+  const months = Math.floor(difference / (1000 * 3600 * 24 * 30));
+  return `${months}m`;
+};
+
+/*
+* Render the list of job offers
+* -----------------------------------------------------------
 */
 
 const renderCards = () => {
@@ -108,54 +172,41 @@ const renderCards = () => {
 };
 
 /*
-* handleSearch() --> xxx 
-* handleLoad()   -->  xxx
+* handleSearch()  
 * -----------------------------------------------------------
 */
-
-const handleSearch = async (event) => {
-  // eslint-disable-next-line no-unused-expressions
-  event.preventDefault();
-  const terms = form.elements.terms.value;
-  const location = form.elements.location.value;
-  const isFullTime = form.elements.jobType.checked;
-  numPage = 1;
-
-  jobs = await getJobs(terms, location, isFullTime);
-  renderCards();
-};
 
 const handleLoad = async () => {
   jobs = await getJobs();
   renderCards();
 };
 
-/*
-* calculateTime() --> 
-* -----------------------------------------------------------
-* Calcula el tiempo transcurrido desde que se publicó la oferta
-* de trabajo.                     
-*/
+const handleSearch = async (e) => {
+  // eslint-disable-next-line no-unused-expressions
+  e.preventDefault();
 
-const calculateTime = (dateCreated) => {
-  const createdAtDate = new Date(dateCreated);
-  const nowDate = new Date();
-  const difference = nowDate - createdAtDate;
+  const terms = form.elements.terms.value;
+  const location = form.elements.location.value;
+  const isFullTime = form.elements.jobType.checked;
 
-  const hours = Math.floor(difference / (1000 * 3600));
-  if (hours < 24) {
-    return `${hours}h`;
-  }
-  const days = Math.floor(difference / (1000 * 3600 * 24));
-  if (days < 7) {
-    return `${days}d`;
-  }
-  const weeks = Math.floor(difference / (1000 * 3600 * 24 * 7));
-  if (weeks < 4) {
-    return `${weeks}w`;
-  }
-  const months = Math.floor(difference / (1000 * 3600 * 24 * 30));
-  return `${months}m`;
+  jobs = await getJobs(terms, location, isFullTime);
+  renderCards();
+};
+
+const handleModalSearch = async (e) => {
+  // eslint-disable-next-line no-unused-expressions
+  e.preventDefault();
+  hideModal();
+
+  const locationModalInput = document.querySelector(".js-modal-location");
+  const typeModalCheckbox = document.querySelector(".js-modal-fullTime");
+
+  const terms = termsInputField.value;
+  const location = locationModalInput.value;
+  const isFullTime = typeModalCheckbox.checked;
+
+  jobs = await getJobs(terms, location, isFullTime);
+  renderCards();
 };
 
 /*
@@ -165,10 +216,7 @@ const calculateTime = (dateCreated) => {
 
 const mediaQuery = window.matchMedia('(max-width: 850px)');
 
-const termsInputField = document.querySelector('.js-terms');
-
 const changePlaceholderText = (query) => {
-
   const typeLabel = document.querySelector('.js-label');
 
   if (query.matches) {
@@ -181,39 +229,11 @@ const changePlaceholderText = (query) => {
   }
 }
 
-const mediaQueryMobile = window.matchMedia('(max-width: 640px)');
-const changeSearchBox = (query) => {
-  const submitBtn = document.querySelector('.js-submit-btn');
-
-  if (query.matches) {
-    submitBtn.innerHTML =
-      `
-      <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg">
-        <path d="M17.112 15.059h-1.088l-.377-.377a8.814 8.814 0 002.15-5.784A8.898 8.898 0 008.898 0 8.898 8.898 0 000 8.898a8.898 8.898 0 008.898 8.899c2.211 0 4.23-.808 5.784-2.143l.377.377v1.081l6.845 6.832 2.04-2.04-6.832-6.845zm-8.214 0A6.16 6.16 0 118.9 2.737a6.16 6.16 0 010 12.322z" fill="#fff" fill-rule="nonzero"/>
-      </svg>
-      `;
-
-  } else {
-    submitBtn.innerHTML = 'Search';
-  }
-}
 changePlaceholderText(mediaQuery);
 mediaQuery.addListener(changePlaceholderText);
 
-changeSearchBox(mediaQueryMobile);
-mediaQueryMobile.addListener(changeSearchBox);
-
-//const loadMoreCards = () => {
-//  jobs = await getJobs();
-//  renderCards();
-//  const hiddenCards = document.querySelectorAll('.hidden');
-//  hiddenCards.forEach((card, index) => {
-//    if (index < maxCards) {
-//      card.classList.remove('hidden');
-//    }
-//  })
-//}
-
+btnOpenModal.addEventListener('click', showModal);
 document.addEventListener("DOMContentLoaded", handleLoad);
-form.addEventListener("submit", handleSearch);
-toggleSwitch.addEventListener("change", switchTheme);
+form.addEventListener('submit', handleSearch);
+toggleSwitch.addEventListener('change', switchTheme);
+btnModalSearch.addEventListener('click', handleModalSearch);
